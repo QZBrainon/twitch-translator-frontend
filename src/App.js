40b74c9349afef15348201bfec4f65ecd45ebcf9
brodrigues0ll <bernardorg99@hotmail.com>
2023-11-21 14:54:29 -0300

    Clean Front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [streamer, setStreamer] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>
+        Streamer:
+        <input
+          type="text"
+          value={streamer}
+          onChange={(e) => setStreamer(e.target.value)}
+        />
+      </label>
+      <button>Connect</button>
+      <ul>
+        {messages.map((msg) => (
+          <li key={msg.username}>
+            {msg.username}:{msg.message}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
