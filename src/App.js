acc175e4f4589d13b5bc5bf3981fcef86eca1430
brodrigues0ll <bernardorg99@hotmail.com>
2023-11-21 22:19:31 -0300

    Organizando e comentando código

--- src/App.js
+++ src/App.js
@@ -1,40 +1,49 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  // State variables
   const [messages, setMessages] = useState([]);
   const [isConnected, setIsConnected] = useState(false);
   const [streamer, setStreamer] = useState("");
   const [prevStreamer, setPrevStreamer] = useState("");
   const [ws, setWs] = useState(null);
 
+  // WebSocket connection useEffect
   useEffect(() => {
     let newWs;
 
     if (isConnected) {
+      // Create a new WebSocket connection
       newWs = new WebSocket("ws://localhost:3001");
 
+      // WebSocket event handlers
       newWs.onopen = () => {
         console.log("WebSocket connection opened");
       };
 
       newWs.onmessage = (event) => {
+        // Handle incoming messages
         const data = JSON.parse(event.data);
         setMessages((prevMessages) => [...prevMessages, data]);
       };
 
       newWs.onclose = () => {
+        // Handle WebSocket connection closure
         console.log("WebSocket connection closed");
         setIsConnected(false);
         setWs(null);
       };
 
+      // Set the new WebSocket connection
       setWs(newWs);
     } else {
+      // Close the WebSocket connection if disconnected
       if (ws) {
         ws.close();
       }
     }
 
+    // Clean up the WebSocket connection on component unmount or dependency change
     return () => {
       if (newWs) {
         newWs.close();
@@ -42,16 +51,21 @@ function App() {
     };
   }, [isConnected, streamer]);
 
+  // useEffect to handle disconnection and update prevStreamer
   useEffect(() => {
     if (prevStreamer && prevStreamer !== streamer && isConnected) {
+      // Disconnect from the previous streamer if the streamer changes
       handleDisconnect(prevStreamer);
     }
 
+    // Update prevStreamer
     setPrevStreamer(streamer);
   }, [streamer, prevStreamer, isConnected]);
 
+  // Function to handle connection
   const handleConnect = async () => {
     try {
+      // Make a POST request to start the WebSocket connection
       const response = await fetch("http://localhost:3001/startConnection", {
         method: "POST",
         headers: {
@@ -61,6 +75,7 @@ function App() {
       });
 
       if (response.ok) {
+        // Set isConnected to true upon successful connection
         setIsConnected(true);
       } else {
         console.error("Failed to connect:", response.statusText);
@@ -70,8 +85,10 @@ function App() {
     }
   };
 
+  // Function to handle disconnection
   const handleDisconnect = async (customStreamer = null) => {
     try {
+      // Make a POST request to stop the WebSocket connection
       const response = await fetch("http://localhost:3001/stopConnection", {
         method: "POST",
         headers: {
@@ -83,6 +100,7 @@ function App() {
       });
 
       if (response.ok) {
+        // Set isConnected to false upon successful disconnection
         setIsConnected(false);
       } else {
         console.error("Failed to disconnect:", response.statusText);
@@ -92,12 +110,15 @@ function App() {
     }
   };
 
+  // Function to clean messages
   const handleClean = () => {
     setMessages([]);
   };
 
+  // JSX for the component
   return (
     <div className="App">
+      {/* Input for entering the streamer name */}
       <label>
         Streamer:
         <input
@@ -106,6 +127,8 @@ function App() {
           onChange={(e) => setStreamer(e.target.value)}
         />
       </label>
+
+      {/* Connect/Disconnect and Clean buttons */}
       {isConnected ? (
         <button onClick={() => handleDisconnect()}>Disconnect</button>
       ) : (
@@ -113,6 +136,7 @@ function App() {
       )}
       <button onClick={handleClean}>Clean</button>
 
+      {/* Display messages in an unordered list */}
       <ul>
         {messages.map((msg, index) => (
           <li key={index}>
